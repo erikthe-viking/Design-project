c09a47882342e08636f4bbffb40593c08d8b7084
Erik Kirkegaard <ekirkegaard12@gmail.com>
2018-10-29 09:10:59 -0500

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -1,2 +1,55 @@
-import numpy
+import jsonrpc 
 
+#access = ServiceProxy("http://user:password@127.0.0.1:8332")
+#access.getinfo()
+#access.listreceivedbyaddress(6)
+
+#conn = dogecoinrpc.connect_to_local() # Connects to local dogecoin wallet instance
+
+
+# Print Balance
+#print "Your balance is %f" % (conn.getbalance(),)
+
+class Player:
+	address = ""
+	bet_ammount = 0
+	winnings = 0
+	win_lose = False
+	
+	def deposit(self):
+		rv = conn.validateaddress(foo)
+		rv.isvalid:
+  		  print "The address that you provided is valid"
+		else:
+  		  print "The address that you provided is invalid, please correct"	
+	 
+	def withdraw():
+		conn.sendtoaddress(address,self.winning) 
+	def stay():
+		pass
+	def deal():
+		pass
+	def calculate_winning():
+		
+		self.winnings = bet_amount * 1.5
+
+
+
+def main():
+	player_1 = Player()
+	player_2 = Player()
+	player_3 = Player()
+
+	# Button Press stubs
+	player_1_deposit = 0:
+	player_2_deposit = 0:
+	player_3_deposit = 0:
+	
+	if player_1_deposit == 1 or player_2_deposit == 1 or player_3_deposit == 1:
+
+	
+		
+	
+
+if __name__ == "__main__":
+    main()
