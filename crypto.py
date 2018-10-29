312e4040812d641b0fcccd639ab33564afcc2a6a
erikthe-viking <ekirkegaard12@gmail.com>
2018-10-29 09:33:53 -0500

    update moar

--- crypto.py
+++ crypto.py
@@ -15,7 +15,8 @@ class Player:
 	bet_ammount = 0
 	winnings = 0
 	win_lose = False
-	
+	gameOver = False
+
 	def deposit(self):
 		rv = conn.validateaddress(foo)
 		rv.isvalid:
@@ -41,17 +42,53 @@ def main():
 	player_3 = Player()
 
 	# Button Press stubs
-	player_1_deposit = 1:
-	player_1_deposit = 1:
-	player_1_deposit = 1:
 	
-
+	# Deposits
 	player_1_deposit = 0:
 	player_2_deposit = 0:
 	player_3_deposit = 0:
-	
-	if player_1_deposit == 1 or player_2_deposit == 1 or player_3_deposit == 1:
 
+	# Deal
+	player_1_deal = 0:
+	player_2_deal = 0:
+	player_3_deal = 0:
+	
+	# Stay
+	player_1_stay = 0:
+	player_2_stay = 0:
+	player_3_stay = 0:
+
+	# Deposit functions
+	if player_1_deposit == 1:
+		player_1.deposit()
+	if player_2_deposit == 1:
+		player_2.deposit()
+	if player_3_deposit == 1:
+		player_3.deposit()
+
+	# Deal
+	if player_1_deal == 1:
+		player_1.deal()
+	if player_2_deal == 1:
+		player_2.deal()
+	if player_3_deal == 1:
+		player_3.deal()
+
+	# Stay
+	if player_1_stay == 1:
+		player_1.stay()
+	if player_2_stay == 1:
+		player_2.stay()
+	if player_3_stay == 1:
+		player_3.stay()
+
+	
+	if player_1.gameOver == True:
+		player_1.withdraw()
+	if player_2_gameOver == True:
+		player_2.withdraw()
+	if player_3_gameOver == True:
+		player_3.withdraw()
 
 
 if __name__ == "__main__":
